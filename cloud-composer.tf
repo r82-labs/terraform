69fa7f464472f6da518a0503af87ec0a3973547d
r82labs-ronan <ronan@r82labs.com>
2025-09-15 21:56:39 +0100

    Create cloud-composer.tf

---
+++ cloud-composer.tf
@@ -0,0 +1,9 @@
+resource "google_composer_environment" "test" {
+  name   = "example-composer-env"
+  region = "us-central1"
+ config {
+    software_config {
+      image_version = "composer-3-airflow-2"
+    }
+  }
+}
