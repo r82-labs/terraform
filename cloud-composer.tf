8a151ce3e24b6de25b34ff6e4c6914fa9cb278f1
r82labs-ronan <ronan@r82labs.com>
2025-09-18 00:04:56 +0100

    Update cloud-composer.tf

--- cloud-composer.tf
+++ cloud-composer.tf
@@ -11,6 +11,12 @@ provider "google" {
   project = var.project_id
 }
 
+resource "google_project_iam_member" "composer_agent_service_account" {
+  project = var.project_id
+  role = "roles/composer.serviceAgent"
+  member = "serviceAccount:service-${var.project_number}@gcp-sa-composer.iam.gserviceaccount.com"
+}
+
 resource "google_composer_environment" "test" {
   name   = "example-composer-env"
   region = "us-central1"
@@ -25,3 +31,6 @@ variable "project_id" {
   description = "Project id"
   default     = "banded-scion-461009-a0"
 }
+variable "project_number" {
+  description = "Project Number"
+  default = "143720609546"
