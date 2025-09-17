51901f31baef1e4fef519e467544be017dba7d7c
r82labs-ronan <ronan@r82labs.com>
2025-09-17 23:20:27 +0100

    Update cloud-composer.tf

--- cloud-composer.tf
+++ cloud-composer.tf
@@ -28,6 +28,14 @@ resource "google_cloud_run_v2_service" "default" {
   }
 }
 
+resource "google_cloud_run_v2_service_iam_member" "member" {
+  project = google_cloud_run_v2_service.default.project
+  location = google_cloud_run_v2_service.default.location
+  name = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
 variable "project_id" {
   description = "Project id"
   default     = "banded-scion-461009-a0"
