09f8537957303c91f0224038e64734afc616cb91
r82labs-ronan <ronan@r82labs.com>
2025-09-17 23:05:40 +0100

    Update cloud-composer.tf

--- cloud-composer.tf
+++ cloud-composer.tf
@@ -29,8 +29,8 @@ resource "google_cloud_run_v2_service" "default" {
 }
 
 resource "google_cloud_run_service_iam_member" "invoker" {
-  service  = google_cloud_run_service.default.name      # The name of the Cloud Run service to grant access to
-  location = google_cloud_run_service.default.location  # The region where the Cloud Run service is deployed
+  service  = google_cloud_run_v2_service.default.name      # The name of the Cloud Run service to grant access to
+  location = google_cloud_run_v2_service.default.location  # The region where the Cloud Run service is deployed
   role     = "roles/run.invoker"                        # The IAM role being granted (allows invoking the service)
   member   = "allUsers"                                 # The identity receiving the role (all users, public access)
 }
